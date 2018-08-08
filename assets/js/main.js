453a4e5a92160460df7465244e02a30db919a9e0
entepsc <42168602+entepsc@users.noreply.github.com>
2018-08-08 10:27:51 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,12 @@ function onClick(element) {
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
 }
+
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
